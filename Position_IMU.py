2730026508caf5686ebb5bbd400bc65dcd3cb5c6
JulienMellet <38491251+JulienMellet@users.noreply.github.com>
2018-07-25 17:37:46 +0200

    Position IMU updates
    The position is allow for an horizontal movement, the gravity vector have to be substracted soon. Orientation matrix have to be known

--- Position_IMU.py
+++ Position_IMU.py
@@ -8,37 +8,67 @@ from math import pi
 from math import *
 from sys import stdin
 import numpy as np
-from reception_IMU import *
+from reception import *
 import time
 
 port = serial_init()
 base, axis, centroids, accelerations = parse_data(port)
+print(parse_data(port))
 
-velocityx = [0,0]
+velocityx = np.zeros(2)
 
 # Period of measurement
 T = 1/120
 
-def position():
-    accelerationx[1] = accelerations[1]
+"""
+def position(accel):
+    accelerationx = np.zeros(2)
+    velocityx = np.zeros(2)
+    positionX = np.zeros(2)
+    
+    accelerationx[1] = accel[1]
 
     # First integration
-    velocityx[1] = velocityx[0] + accelerationx[0] + ((accelerationx[1] - accelerationx[0])/2) * T
+    velocityx[1] = velocityx[0] + accelerationx[0] * 1/120
     # Second integration
-    positionX[1] = positionX[0] + velocityx[0] + ((velocityx[1] - velocityx[0])/2) * T
+    positionX[1] = positionX[0] + velocityx[0] * 1/120
     
     return positionX[1]
     
     accelerationx[0] = accelerationx[1]
     velocityx[0] = velocityx[1]
     positionX[0] = positionX[1]
+"""
 
-    
-    
+accelerationX = np.zeros(2)
+velocityX = np.zeros(2)
+positionX = np.zeros(2) 
 
 while 1 :
     #print(parse_data(port))
-    position()
-    print(position())
+    base, axis, centroids, accelerations = parse_data(port)
     
-    time.sleep(1/120)    
+    #print(accelerations)
+    
+    accelerationX[1] = + accelerations[1]
+    #accelerationY[1] = - accelerations[0]
+    # accelerationZ[1] = - accelerations[2]
+
+    # First integration
+    velocityX[1] = velocityX[0] + accelerationX[0] * 1/120
+    # Second integration
+    positionX[1] = positionX[0] + velocityX[0] * 1/120
+    
+    #print(positionX[1])
+    
+    # Update previous values
+    accelerationX[0] = accelerationX[1]
+    velocityX[0] = velocityX[1]
+    positionX[0] = positionX[1]    
+    
+    
+    #position(accelerations)
+    #print(position(accelerations))
+    
+    #print("time = ",time.clock()," s" )
+    print(time.clock())
